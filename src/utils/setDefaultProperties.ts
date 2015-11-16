f4a4bdeaca0d1fa8946ea189aba5af318000ffe2
Poul Kjeldager Sørensen <pks@s-innovations.net>
2015-11-16 23:12:45 +0100

    making use of isDefined to fix issue with false bool

--- src/utils/setDefaultProperties.ts
+++ src/utils/setDefaultProperties.ts
@@ -1,10 +1,11 @@
 
 import makeObservable = require("../koExtensions/makeObservable");
+import isDefined = require("./isDefined");
 
 function setDefaultProperties(obj: any, props, defaults, mapper?) {
     for (var key in defaults) {
-        
-        var value = props[key] || defaults[key];
+
+        var value = isDefined(props[key]) ? props[key] : defaults[key];
         obj[key] = makeObservable(mapper && mapper[key] ? mapper[key](value) : value);
     }
 }
