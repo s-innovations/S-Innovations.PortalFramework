b695ae51e6af3732cb1cb2dfdad08276e1efa8d5
Poul Kjeldager Sørensen <pks@s-innovations.net>
2015-12-16 23:55:27 +0100

    refactored fix again

--- dist/src/koExtensions/verboseConsoleBindingErrorReporter.js
+++ dist/src/koExtensions/verboseConsoleBindingErrorReporter.js
@@ -1,33 +1,20 @@
 define(["require", "exports", "knockout"], function (require, exports, ko) {
     var existing = ko.bindingProvider.instance;
-    existing.getBindings = function (node, bindingContext) {
-        var bindings;
-        try {
-            bindings = existing.getBindings(node, bindingContext);
-        }
-        catch (ex) {
-            if (window.console && console.log) {
-                console.log("binding error", ex.message, node, bindingContext);
+    ko.bindingProvider.instance = {
+        nodeHasBindings: existing.nodeHasBindings,
+        getBindings: function (node, bindingContext) {
+            var bindings;
+            try {
+                bindings = existing.getBindings(node, bindingContext);
             }
+            catch (ex) {
+                if (window.console && console.log) {
+                    console.log("binding error", ex.message, node, bindingContext);
+                }
+            }
+            return bindings;
         }
-        return bindings;
     };
+    ko.bindingProvider.instance["preprocessNode"] = existing["preprocessNode"];
 });
-//ko.bindingProvider.instance = {
-//    nodeHasBindings: existing.nodeHasBindings,
-//    getBindingAccessors: existing.getBindingAccessors,
-//    getBindings: function (node, bindingContext) {
-//        var bindings;
-//        try {
-//            bindings = existing.getBindings(node, bindingContext);
-//        }
-//        catch (ex) {
-//            if (window.console && console.log) {
-//                console.log("binding error", ex.message, node, bindingContext);
-//            }
-//        }
-//        return bindings;
-//    }
-//};
-//ko.bindingProvider.instance["preprocessNode"] = existing["preprocessNode"];
 //# sourceMappingURL=verboseConsoleBindingErrorReporter.js.map
--- src/koExtensions/verboseConsoleBindingErrorReporter.ts
+++ src/koExtensions/verboseConsoleBindingErrorReporter.ts
@@ -6,35 +6,21 @@ import ko = require("knockout");
 
     var existing = ko.bindingProvider.instance;
 
-    existing.getBindings = function (node, bindingContext) {
-        var bindings;
-        try {
-            bindings = existing.getBindings(node, bindingContext);
-        }
-        catch (ex) {
-            if (window.console && console.log) {
-                console.log("binding error", ex.message, node, bindingContext);
+    ko.bindingProvider.instance = {
+        nodeHasBindings: existing.nodeHasBindings,    
+        getBindings: function (node, bindingContext) {
+            var bindings;
+            try {
+                bindings = existing.getBindings(node, bindingContext);
             }
+            catch (ex) {
+                if (window.console && console.log) {
+                    console.log("binding error", ex.message, node, bindingContext);
+                }
+            }
+
+            return bindings;
         }
-
-        return bindings;
     };
-    //ko.bindingProvider.instance = {
-    //    nodeHasBindings: existing.nodeHasBindings,
-    //    getBindingAccessors: existing.getBindingAccessors,
-       
-    //    getBindings: function (node, bindingContext) {
-    //        var bindings;
-    //        try {
-    //            bindings = existing.getBindings(node, bindingContext);
-    //        }
-    //        catch (ex) {
-    //            if (window.console && console.log) {
-    //                console.log("binding error", ex.message, node, bindingContext);
-    //            }
-    //        }
 
-    //        return bindings;
-    //    }
-    //};
-    //ko.bindingProvider.instance["preprocessNode"] = existing["preprocessNode"];
+    ko.bindingProvider.instance["preprocessNode"] = existing["preprocessNode"];
