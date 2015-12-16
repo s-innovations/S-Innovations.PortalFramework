07d3b7f853b7a9c0c4646c5b1df395290ec18546
Poul Kjeldager Sørensen <pks@s-innovations.net>
2015-12-16 23:38:37 +0100

    fixed verboseConsoleBinding provider

--- src/koExtensions/verboseConsoleBindingErrorReporter.ts
+++ src/koExtensions/verboseConsoleBindingErrorReporter.ts
@@ -6,22 +6,35 @@ import ko = require("knockout");
 
     var existing = ko.bindingProvider.instance;
 
-    ko.bindingProvider.instance = {
-        nodeHasBindings: existing.nodeHasBindings,
-        getBindingAccessors: existing.getBindingAccessors,
-       
-        getBindings: function (node, bindingContext) {
-            var bindings;
-            try {
-                bindings = existing.getBindings(node, bindingContext);
-            }
-            catch (ex) {
-                if (window.console && console.log) {
-                    console.log("binding error", ex.message, node, bindingContext);
-                }
-            }
-
-            return bindings;
+    existing.getBindings = function (node, bindingContext) {
+        var bindings;
+        try {
+            bindings = existing.getBindings(node, bindingContext);
         }
+        catch (ex) {
+            if (window.console && console.log) {
+                console.log("binding error", ex.message, node, bindingContext);
+            }
+        }
+
+        return bindings;
     };
-    ko.bindingProvider.instance["preprocessNode"] = existing["preprocessNode"];
+    //ko.bindingProvider.instance = {
+    //    nodeHasBindings: existing.nodeHasBindings,
+    //    getBindingAccessors: existing.getBindingAccessors,
+       
+    //    getBindings: function (node, bindingContext) {
+    //        var bindings;
+    //        try {
+    //            bindings = existing.getBindings(node, bindingContext);
+    //        }
+    //        catch (ex) {
+    //            if (window.console && console.log) {
+    //                console.log("binding error", ex.message, node, bindingContext);
+    //            }
+    //        }
+
+    //        return bindings;
+    //    }
+    //};
+    //ko.bindingProvider.instance["preprocessNode"] = existing["preprocessNode"];
