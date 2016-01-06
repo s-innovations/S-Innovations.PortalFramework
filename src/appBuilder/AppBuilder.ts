e351cdddb6b02704fe3a04c551b67e2a5b661655
Poul Kjeldager Sørensen <pks@s-innovations.net>
2016-01-06 21:01:16 +0100

    Added a AppBuilder implementation for middlewares

---
+++ src/appBuilder/AppBuilder.ts
@@ -0,0 +1,53 @@
+import { compose } from './compose'
+import * as Promise from "q";
+
+interface AppEnvironmnet {
+    hash: string
+    originalHash: string
+    route: string[];
+    params: string;
+    [key: string]: any;
+}
+
+interface AppFunc {
+    (env: AppEnvironmnet): void;
+}
+
+interface Middleware {
+    (env: AppEnvironmnet, next: AppFunc): void | Promise.IPromise<void>;
+}
+
+class AppBuilder<T extends AppEnvironmnet> {
+
+    private middleware = []
+    constructor() {
+
+    }
+
+    build() {
+        if (!this.middleware.length) {
+            throw new Error('Usage error: must have at least one middleware')
+        }
+        return compose(this.middleware)
+    }
+
+    use(mw) {
+        if ('function' !== typeof mw) {
+            throw new TypeError(`${mw}, middleware must be a function`)
+        }
+        this.middleware.push(mw)
+        return this
+    }
+}
+
+export default function <T extends AppEnvironmnet>() {
+    return new AppBuilder<T>()
+}
+
+export {
+compose,
+AppBuilder,
+AppFunc,
+AppEnvironmnet,
+Middleware
+}
